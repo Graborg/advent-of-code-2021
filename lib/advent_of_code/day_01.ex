ab9c094b52275217eea2bf5aa0c9ad4e5e0f5c1d
Mikael Gråborg <mikael.graborg@iteam.se>
2021-12-01 00:12:39 -0600

    add day1p2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -8,5 +8,19 @@ defmodule AdventOfCode.Day01 do
   end
 
   def part2(args) do
+
+    Enum.map_reduce(args, [], &part2_reducer/2) 
+    |> elem(0)
+    |> tl()
+    |> Enum.filter(fn x -> x end)
+    |> Enum.count()
   end
+
+  def part2_reducer(x, [_first, second, third] = old_win) do
+      new_win = [second, third, x] 
+    {Enum.sum(new_win) > Enum.sum(old_win), new_win}
+  end
+
+  def part2_reducer(x, old_win), do: {false, old_win ++ [x]} 
+
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,6 +3,7 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
+  @tag :skip
   test "part1" do
     input = """
             199 
@@ -23,6 +24,7 @@ defmodule AdventOfCode.Day01Test do
     assert result == 7
   end
 
+  @tag :skip
   test "part1withInput" do
     input = AdventOfCode.Input.get!(1, 2021)
             |>String.split("\n", trim: true)
@@ -31,11 +33,36 @@ defmodule AdventOfCode.Day01Test do
             |>part1()
             |>IO.inspect(label: "\n Answer d01p01")
   end
+
   @tag :skip
   test "part2" do
-    input = AdventOfCode.Input.get!(1)
+    input = """
+            199 
+            200 
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            """
+            |>String.split("\n", trim: true)
+            |>Enum.map(&String.trim/1)
+            |>Enum.map(&String.to_integer/1)
     result = part2(input)
 
-    assert result
+    assert result == 5
+  end
+
+  @tag :skip
+  test "part2withInput" do
+    input = AdventOfCode.Input.get!(1, 2021)
+            |>String.split("\n", trim: true)
+            |>Enum.map(&String.trim/1)
+            |>Enum.map(&String.to_integer/1)
+            |>part2()
+            |>IO.inspect(label: "\n Answer d01p02")
   end
 end
