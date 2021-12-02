4504c51c1e1206a435ef5e155ddeac76b5473175
Mikael Gråborg <mikael.graborg@iteam.se>
2021-12-01 23:55:07 -0600

    add day2p2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,5 @@
 defmodule AdventOfCode.Day02 do
-  def part1(args) do
-    Enum.reduce(args, {0,0}, &handle_command/2)
-  end
+  def part1(args), do: Enum.reduce(args, {0,0}, &handle_command/2)
 
   def handle_command("forward " <> x, {hpos, dpos}), do: {hpos + String.to_integer(x), dpos}
   
@@ -9,8 +7,12 @@ defmodule AdventOfCode.Day02 do
 
   def handle_command("up " <> x, {hpos, dpos}), do: {hpos, dpos - String.to_integer(x)}
 
-  def part2(args) do
 
+  def part2(args), do: Enum.reduce(args, {0,0,0}, &handle_command2/2)
 
-  end
+  def handle_command2("forward " <> x, {hpos, dpos, aim}), do: {hpos + String.to_integer(x), dpos + aim * String.to_integer(x), aim}
+  
+  def handle_command2("down " <> x, {hpos, dpos, aim}), do: {hpos, dpos, aim + String.to_integer(x)}
+
+  def handle_command2("up " <> x, {hpos, dpos, aim}), do: {hpos, dpos, aim - String.to_integer(x)}
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -21,13 +21,14 @@ defmodule AdventOfCode.Day02Test do
     assert result == 150 
   end
 
+  @tag :skip
   test "part1withInput" do
     input = AdventOfCode.Input.get!(2, 2021)
             |>String.split("\n", trim: true)
             |>Enum.map(&String.trim/1)
     {hpos, dpos} = part1(input)
     result = hpos * dpos
-    IO.inspect(result, label: "\n Answer d01p01")
+    IO.inspect(result, label: "\n Answer d02p01")
   end
 
   @tag :skip
@@ -42,9 +43,18 @@ defmodule AdventOfCode.Day02Test do
             """
             |>String.split("\n", trim: true)
             |>Enum.map(&String.trim/1)
-    {hpos, dpos} = part1(input)
+    {hpos, dpos, _aim} = part2(input)
             |> IO.inspect()
     result = hpos * dpos
-    assert result == 150 
+    assert result == 900 
+  end
+
+  test "part2withInput" do
+    input = AdventOfCode.Input.get!(2, 2021)
+            |>String.split("\n", trim: true)
+            |>Enum.map(&String.trim/1)
+    {hpos, dpos, _aim} = part2(input)
+    result = hpos * dpos
+    IO.inspect(result, label: "\n Answer d02p01")
   end
 end
