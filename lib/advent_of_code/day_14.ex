f4a6f8258c9000a254fed987eaa930c7d018e521
Mikael Gråborg <mikael.graborg@iteam.se>
2021-12-14 01:18:10 -0600

    add day14part1 and try to optimize

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,7 +1,97 @@
 defmodule AdventOfCode.Day14 do
-  def part1(args) do
+  def part1([template | insertion_rules]) do
+    temp_list = Enum.chunk_every(String.split(template, "", trim: true), 2, 1, :discard)
+
+    values =
+      insert_rules(
+        temp_list,
+        Enum.map(insertion_rules, fn rule -> String.split(rule, " -> ", trim: true) end),
+        10,
+        %{}
+      )
+      |> get_string_from_chunks()
+      |> Enum.join()
+      |> Kernel.<>(String.last(template))
+      |> IO.inspect()
+      |> String.split("", trim: true)
+      |> Enum.group_by(fn x -> x end)
+      |> Map.values()
+      |> Enum.map(&length/1)
+      |> Enum.sort()
+      |> IO.inspect()
+
+    List.last(values) - List.first(values)
   end
 
-  def part2(args) do
+  def get_string_from_chunks(chunks) do
+    Enum.map(chunks, fn [start, _other] -> start end)
+  end
+
+  def insert_rules(template, _rules, 0, _), do: template
+
+  def insert_rules(temp_list, rules, count, map) do
+    IO.inspect(Enum.join(temp_list))
+    IO.inspect(count, label: "count")
+
+    {new_temp_list, new_map} =
+      temp_list
+      |> IO.inspect(label: "chinked")
+      |> Enum.reduce({[], map}, fn [start, other], {acc_list, temp_map} ->
+        if Map.has_key?(temp_map, [start, other]) do
+          new = Map.get(temp_map, [start, other])
+          {acc_list ++ [[start, new], [new, other]], temp_map}
+        else
+          [_, new] =
+            Enum.find(rules, fn [from, _] ->
+              String.starts_with?(from, start <> other)
+            end)
+
+          {acc_list ++ [[start, new], [new, other]], Map.put_new(temp_map, [start, other], new)}
+        end
+      end)
+
+    insert_rules(new_temp_list, rules, count - 1, new_map)
+  end
+
+  def part21([template | insertion_rules]) do
+    values =
+      insert_rules(
+        template,
+        Enum.map(insertion_rules, fn rule -> String.split(rule, " -> ", trim: true) end),
+        40,
+        %{}
+      )
+      |> String.split("", trim: true)
+      |> Enum.group_by(fn x -> x end)
+      |> Map.values()
+      |> Enum.map(&length/1)
+      |> Enum.sort()
+      |> IO.inspect()
+
+    List.last(values) - List.first(values)
+  end
+
+  def part2([template | insertion_rules]) do
+    temp_list = Enum.chunk_every(String.split(template, "", trim: true), 2, 1, :discard)
+
+    values =
+      insert_rules(
+        temp_list,
+        Enum.map(insertion_rules, fn rule -> String.split(rule, " -> ", trim: true) end),
+        40,
+        %{}
+      )
+      |> get_string_from_chunks()
+      |> Enum.join()
+      |> Kernel.<>(String.last(template))
+      |> IO.inspect()
+      |> String.split("", trim: true)
+      |> Enum.group_by(fn x -> x end)
+      |> Map.values()
+      |> Enum.map(&length/1)
+      |> Enum.sort()
+      |> IO.inspect()
+
+    List.last(values) - List.first(values)
   end
 end
--- test/advent_of_code/day_14_test.exs
+++ test/advent_of_code/day_14_test.exs
@@ -5,17 +5,75 @@ defmodule AdventOfCode.Day14Test do
 
   @tag :skip
   test "part1" do
-    input = nil
+    input =
+      """
+      NNCB
+
+      CH -> B
+      HH -> N
+      CB -> H
+      NH -> C
+      HB -> C
+      HC -> B
+      HN -> C
+      NN -> C
+      BH -> H
+      NC -> B
+      NB -> B
+      BN -> B
+      BB -> N
+      BC -> B
+      CC -> N
+      CN -> C
+      """
+      |> String.split("\n", trim: true)
+      |> IO.inspect()
+
     result = part1(input)
 
-    assert result
+    # assert result == "NCNBCHB"
+    #  assert result == "NBCCNBBBCBHCB"
+    assert result == 1588
   end
 
   @tag :skip
+  test "part1withInput" do
+    input =
+      AdventOfCode.Input.get!(14, 2021)
+      |> String.split("\n", trim: true)
+
+    result = part1(input)
+    IO.inspect(result, label: "\n Answer d14p01")
+    assert result == 3306
+  end
+
   test "part2" do
-    input = nil
+    input =
+      """
+      NNCB
+
+      CH -> B
+      HH -> N
+      CB -> H
+      NH -> C
+      HB -> C
+      HC -> B
+      HN -> C
+      NN -> C
+      BH -> H
+      NC -> B
+      NB -> B
+      BN -> B
+      BB -> N
+      BC -> B
+      CC -> N
+      CN -> C
+      """
+      |> String.split("\n", trim: true)
+      |> IO.inspect()
+
     result = part2(input)
 
-    assert result
+    assert result == 2_188_189_693_529
   end
 end
